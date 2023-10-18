4767e913ecf3b709ac7cfe298e2d91becd1b54f0
Mahsa <mahskh08@gmail.com>
2023-10-18 16:12:26 +0430

    method_chaining

---
+++ method_chaining.js
@@ -0,0 +1,7 @@
+// Solution
+
+let tweet = 'Starting to get the hang of it... #javascript #launchschool';
+
+tweet.split(' ');                        // ['Starting', 'to', 'get', 'the', 'hang', 'of', 'it...', '#javascript', '#launchshcool']
+tweet.split(' ').reverse();              // ['#launchschool', '#javascript', 'it...', 'of', 'hang', 'the', 'get', 'to', 'Starting']
+tweet.split(' ').reverse().join(' ');    // 'Starting to get the hang of it... #javascript #launchschool';
