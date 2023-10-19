ddc0134fdeb76bf5ce7c120879ae997b4936a50d
Mahsa <mahskh08@gmail.com>
2023-10-19 19:39:40 +0430

    primitive types and JavaScript code style

---
+++ read_documentation2/style_guide.js
@@ -0,0 +1,22 @@
+// Bad or invalid style in JavaScript coding
+let ice_cream_taste = 'chocolate'
+let ice_cream_density = 10
+
+while(ice_cream_density > 0)
+{
+    console.log('Drip...');
+    ice_cream_density -= 1;
+}
+
+console.log('The '+ ice_cream_taste +' ice cream melted.');
+
+// Good or valid JavaScript style
+let iceCreamTaste = 'chocolate';
+let iceCreamDensity = 10;
+
+while (iceCreamDensity > 0) {
+  console.log('Drip...');
+  iceCreamDensity -= 1;
+}
+
+console.log('The ' + iceCreamTaste + ' ice cream melted.');
