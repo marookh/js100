e67a108bcbf6664939860efcb6f85681e57b823e
Mahsa <mahskh08@gmail.com>
2023-10-23 21:40:27 +0430

    writing while loop

---
+++ loops/that_odd.js
@@ -0,0 +1,18 @@
+// Write a while loop that logs all odd natural numbers between 1 and 40.
+let counter = 1
+while (counter < 40) {
+    if (counter % 2 === 1) {
+        console.log(counter)
+    } 
+    counter++;
+}
+
+
+// or
+
+let num = 1;
+
+while (num < 40) {
+  console.log(num);
+  num += 2;
+}
