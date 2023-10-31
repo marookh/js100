631d8d8a40b0f96e1eaddd2b35a63078e2ebf777
Mahsa <mahskh08@gmail.com>
2023-10-31 21:17:29 +0430

    method chaining

--- read_documentation/method_chaining.js
+++ read_documentation/method_chaining.js
@@ -5,3 +5,4 @@ let tweet = 'Starting to get the hang of it... #javascript #launchschool';
 tweet.split(' ');                        // ['Starting', 'to', 'get', 'the', 'hang', 'of', 'it...', '#javascript', '#launchshcool']
 tweet.split(' ').reverse();              // ['#launchschool', '#javascript', 'it...', 'of', 'hang', 'the', 'get', 'to', 'Starting']
 tweet.split(' ').reverse().join(' ');    // 'Starting to get the hang of it... #javascript #launchschool';
+
