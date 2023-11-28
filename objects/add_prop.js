7dfaa1c30e731ee029fd7e9f3c08c37fedffda26
Mahsa <mahskh08@gmail.com>
2023-11-28 16:30:44 +0430

    adding property to object

---
+++ objects/add_prop.js
@@ -0,0 +1,13 @@
+// Below is a simple object representing our dog, Fido. 
+// On lines 11 and 12, write code to add properties 'age' and 'favorite food' to the fido object.
+
+let fido = {
+    name: 'Fido',
+    species: 'Labrador Retriever',
+    color: 'brown',
+    weight: 16,
+  };
+  
+  fido['age'] = 4;
+  fido['favorite food'] = 'peanut butter';
+  console.log(fido);
